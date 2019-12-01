9c494f4ddc015e2b938922d56782dc46c90e8fdc
Binyamin Manela <39960472+manelabinyamin@users.noreply.github.com>
2019-12-01 19:42:55 +0200

    add submission funtion

--- LoadData.py
+++ LoadData.py
@@ -17,3 +17,19 @@ def load_Kaggle2Collab(comp_api):
     # Copy the desired data set locally. Go to the competition's paga, Data tab and copy the API
     # !kaggle competitions do
     os.system(comp_api)
+    
+    kaggle competitions download -c nfl-big-data-bowl-2020
+
+def submit_prediction(comp_submission_api):
+    files.upload()
+
+    # Next, install the Kaggle API client.
+    os.system('pip install -q kaggle')
+    # The Kaggle API client expects this file to be in ~/.kaggle,
+    # so move it there.
+    os.system('mkdir -p ~/.kaggle')
+    os.system('cp kaggle.json ~/.kaggle/')
+
+    # This permissions change avoids a warning on Kaggle tool startup.
+    os.system('chmod 600 ~/.kaggle/kaggle.json')
+    os.system(comp_submission_api)
