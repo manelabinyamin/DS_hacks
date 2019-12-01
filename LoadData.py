36246b901f44796f460eaab20b777fc118fed614
Binyamin Manela <39960472+manelabinyamin@users.noreply.github.com>
2019-12-01 18:14:29 +0200

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -1,18 +1,18 @@
 from google.colab import files
 import pandas as pd
+import os
 
 def load_Kaggle2Collab(comp_api):
     files.upload()
 
     # Next, install the Kaggle API client.
-    !pip install -q kaggle
+    os.system('pip install -q kaggle')
     # The Kaggle API client expects this file to be in ~/.kaggle,
     # so move it there.
-    !mkdir -p ~/.kaggle
-    !cp kaggle.json ~/.kaggle/
+    os.system('mkdir -p ~/.kaggle')
+    os.system('cp kaggle.json ~/.kaggle/')'
 
     # This permissions change avoids a warning on Kaggle tool startup.
-    !chmod 600 ~/.kaggle/kaggle.json
+    os.system('chmod 600 ~/.kaggle/kaggle.json')
     # Copy the desired data set locally. Go to the competition's paga, Data tab and copy the API
-    # !kaggle competitions download -c titanic
-    !$comp_api
+    # !kaggle competitions do
