8f3711da65ced7d9a8e64cec41022ef1885f9d01
Binyamin Manela <39960472+manelabinyamin@users.noreply.github.com>
2019-12-01 19:49:09 +0200

    Update and rename KaggleCollab.py to KaggleWithNotebook.py

--- KaggleWithNotebook.py
+++ KaggleWithNotebook.py
@@ -2,7 +2,7 @@ from google.colab import files
 import pandas as pd
 import os
 
-def load_Kaggle2Collab(comp_api):
+def load_Kaggle2Collab(comp_data_api):
     files.upload()
 
     # Next, install the Kaggle API client.
