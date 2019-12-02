557dde2a19abff4c48d0e34eaa82fe11fb202e3d
Binyamin Manela <39960472+manelabinyamin@users.noreply.github.com>
2019-12-02 14:56:44 +0200

    Update FeatureEngineering.py

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -1,7 +1,9 @@
 from scipy.stats import rankdata
 import numpy as np
 
-def feature_normalizer(x):
-    dist_data = np.sort(np.random.normal(loc=np.mean(x), scale=np.std(x), size=len(x)))
+def feature_normalizer(x, mean=None, std=None):
+    mean = np.mean(x) if mean is None else mean
+    std = np.std(x) if std is None else std
+    dist_data = np.sort(np.random.normal(loc=mean, scale=std, size=len(x)))
     sort_idx = rankdata(x, method='min') - 1
     return dist_data[sort_idx]
