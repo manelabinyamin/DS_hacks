f9601f5e6de3894140214be43fa67d1c6b4fd644
Binyamin Manela <39960472+manelabinyamin@users.noreply.github.com>
2019-12-03 15:43:15 +0200

    work on titanic

--- example/titanic example.py
+++ example/titanic example.py
@@ -29,7 +29,7 @@ clean_data = DataCleaning.clean_nans(all_data, method='best_predictor', feature_
 features_to_bin = ['Age','Fare','Parch', 'SibSp']
 rules = {}
 for f in features_to_bin:
-    rules[f] = FeatureEngineering.get_best_binning_rules(clean_data.head(train_size)[f], train['Survived'], target_type='categorical')
+    rules[f] = FeatureEngineering.get_best_binning_rules(clean_data.head(train_size)[f], train['Survived'], target_type='classification')
 for f,bins in rules.items():
     clean_data['binned_'+f] = pd.cut(clean_data[f], bins=bins, labels=False)
 
@@ -39,17 +39,23 @@ clean_data['Sex'] = clean_data['Sex'].astype(float)
 # set Embarked to numbers
 clean_data['Embarked'] = clean_data['Embarked'].map({'C': 0, 'S': 1, 'Q':2})
 
-# plot features
-binned_train = clean_data[['Pclass','Sex','binned_Age','binned_Fare','binned_Parch','binned_SibSp','Embarked']]
-binned_train = pd.concat((binned_train.head(train_size), train['Survived']), axis=1)
-for f in binned_train.keys():
-    EDA.plot_trend_ordinal_over_feature('Survived', f, binned_train, order=3)
-plt.show()
-
-
 # build binary features
 features_to_1hot = ['Pclass','binned_Age','binned_Fare','binned_Parch', 'binned_SibSp', 'Embarked']
 clean_data = FeatureEngineering.build_binary_features(clean_data, features_to_1hot)
+
+# plot features
+cat_features = ['Pclass','Sex','binned_Age','binned_Fare','binned_Parch','binned_SibSp','Embarked']
+cat_data = clean_data[cat_features]
+cat_data = pd.concat((cat_data.head(train_size), train['Survived']), axis=1)
+for f in cat_data.keys():
+    EDA.plot_trend_ordinal_over_feature('Survived', f, cat_data, order=3)
+
+ordinal_features = ['Pclass', 'binned_Age', 'binned_Fare', 'binned_Parch', 'binned_SibSp',
+                    'Embarked_0', 'Embarked_1', 'Embarked_2']
+ordinal_data = clean_data[ordinal_features]
+ordinal_data = pd.concat((ordinal_data.head(train_size), train['Survived']), axis=1)
+EDA.corrplot(ordinal_data.corr())
+plt.show()
 
 # split to train and test
 features_to_drop = ['PassengerId', 'Pclass', 'Name', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Embarked',
@@ -63,7 +69,6 @@ print('---------------------------------------------------------------------')
 print('Final features:')
 train_x.info()
 
-'''
 # train model
 print('---------------------------------------------------------------------')
 print('Cross-validation scores:')
@@ -80,6 +85,7 @@ for m,clf in models.items():
     cv_score = cross_val_score(clf, train_x, train_y, cv=10)
     scores.loc[m,:] = np.percentile(cv_score, [0,25,50,75,100])
 
+print('Percentile validation score')
 print(scores.head())
 best_model = scores['25%'].idxmax()
 print('Best model: ',best_model)
@@ -90,4 +96,3 @@ clf = models[best_model]
 clf.fit(train_x, train_y)
 DS_hacks_submission['Survived'] = clf.predict(test_x)
 DS_hacks_submission.to_csv('DS_hacks_submission.csv', index=False)
-'''
